90bf524059c49d7fb017393fa37ea1e9ff670840
Aragorn <>
2025-04-22 21:13:37 +0300

    Changed back to dark theme

--- config.php
+++ config.php
@@ -4,8 +4,3 @@ $host = "162.241.217.237";
 $dbname = "rmbouimy_vladikDatabase";
 $user = "rmbouimy_vladik";
 $pass = "BROWN69aboba";
-
-$test = "modyfing file to test git";
-<<<<<<< HEAD
-$test = "resolved conflict between experimental and main branches :(";
->>>>>>> experimental
