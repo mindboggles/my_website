fb1ca4b1edfad1d0021eaa1f1ff236154d635e2b
Aragorn <>
2025-04-28 16:03:51 +0300

    not bad

--- index.php
+++ index.php
@@ -14,11 +14,17 @@
 
 		<main>
 			<ol>
-				<li><a href="product-listing-page">Product listing page</a></li>
+				<!-- <li><a href="product-listing-page">Product listing page</a></li>
 				<li><a href="carb-calculator">Nutrition Calculator 1.0</a></li>
 				<li><a href="my-nutrition">Nutrition Calculator 2.0</a></li>
 				<li><a href="login-logout">Login-logout</a></li>
-				<li><a href="placeholder.php">...</a></li>
+				<li><a href="placeholder.php">...</a></li> -->
+
+				<li><button onclick="location.assign('product-listing-page');">Product listing page</button></li>
+				<li><button onclick="location.assign('carb-calculator');">Nutrition calculator 1.0</button></li>
+				<li><button onclick="location.assign('my-nutrition');">Nutrition calculator 2.0</button></li>
+				<li><button onclick="location.assign('login-logout');">Login-logout</button></li>
+				<li><button onclick="location.assign('placeholder');">..........</button></li>
 			</ol>
 		</main>
 
